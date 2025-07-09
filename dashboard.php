06287072d1a9b443acca922d6d251aaad4d858fa
Sameera Perera <sameerasc85@gmail.com>
2025-07-09 08:06:14 +0530

    Update page title to 'Dashboard'
    Changed the <title> from ' to 'Dashboard'

--- dashboard.php
+++ dashboard.php
@@ -27,7 +27,7 @@ function renderModules($conn, $year, $semester) {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Horizontal Levels + Vertical Semesters</title>
+  <title>Dashboard</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
